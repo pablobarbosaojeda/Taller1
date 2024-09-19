fbf8e362bfa76220f977a644c27b6b1e469c0ac9
Barbosa <pbarboje@myuax.com>
2024-09-19 19:24:03 +0200

    Desarrollo de la logíca de la actividad principal de la app

--- app/src/main/java/com/example/taller1/PrincipalActivity.kt
+++ app/src/main/java/com/example/taller1/PrincipalActivity.kt
@@ -1,4 +1,28 @@
 package com.example.taller1
 
-class PrincipalActivity {
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class PrincipalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val displayTextView = findViewById<TextView>(R.id.displayTextView)
+        val goToConfigButton = findViewById<Button>(R.id.goToConfigButton)
+
+        saveButton.setOnClickListener {
+            val userName = nameEditText.text.toString()
+            displayTextView.text = userName
+        }
+
+    }
 }
